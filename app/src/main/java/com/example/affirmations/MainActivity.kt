bd0bd1991cfb2250bdcdfbf095948dc6042dd9a8
melumilic <melumilic@gmail.com>
2023-01-17 17:02:24 -0800

    Add code to use GridLayout

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.affirmations
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.affirmations.adapter.ItemAdapter
 import com.example.affirmations.data.DataSource
@@ -15,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         val myDataSet = DataSource().loadAffirmations()
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
         recyclerView.adapter = ItemAdapter(this, myDataSet)
-
+        //recyclerView.layoutManager = GridLayoutManager(this,3)
         recyclerView.setHasFixedSize(true)
     }
 }
